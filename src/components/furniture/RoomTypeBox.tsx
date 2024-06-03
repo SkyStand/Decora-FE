335e90e732f84d92c533260316112e416cf54fab
Clarice-Wijaya <clarice.wijaya@binus.edu>
2024-06-04 00:39:27 +0700

    feat: Tabs Product

--- src/components/furniture/RoomTypeBox.tsx
+++ src/components/furniture/RoomTypeBox.tsx
@@ -26,37 +26,39 @@ const Card: React.FC<CardProps> = ({ title, body, backgroundImage, backgroundPos
 
 const RoomTypeBox: React.FC = () => {
   return (
-    <div className="container mt-96 mb-40 font-worksans">
-      <Card 
-        title="LIVING ROOM" 
-        body="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio, culpa." 
-        backgroundImage="/images/furniture/livingroom.png" 
-        backgroundPosition="-250px"
-      />
-      <Card 
-        title="BEDROOM" 
-        body="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio, culpa." 
-        backgroundImage="/images/furniture/bedroom.png" 
-        backgroundPosition="-100px"
-      />
-      <Card 
-        title="DINING ROOM" 
-        body="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio, culpa." 
-        backgroundImage="/images/furniture/diningroom.png" 
-        backgroundPosition="-240px"
-      />
-      <Card 
-        title="OFFICE" 
-        body="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio, culpa." 
-        backgroundImage="/images/furniture/office.png" 
-        backgroundPosition="-340px"
-      />
-      <Card 
-        title="OUTDOOR" 
-        body="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio, culpa." 
-        backgroundImage="/images/furniture/outdoor.png" 
-        backgroundPosition="-500px"
-      />
+    <div className='w-full flex justify-center'>
+      <div className="container mt-96 mb-40 font-worksans">
+        <Card 
+          title="LIVING ROOM" 
+          body="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio, culpa." 
+          backgroundImage="/images/furniture/livingroom.png" 
+          backgroundPosition="-250px"
+        />
+        <Card 
+          title="BEDROOM" 
+          body="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio, culpa." 
+          backgroundImage="/images/furniture/bedroom.png" 
+          backgroundPosition="-100px"
+        />
+        <Card 
+          title="DINING ROOM" 
+          body="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio, culpa." 
+          backgroundImage="/images/furniture/diningroom.png" 
+          backgroundPosition="-240px"
+        />
+        <Card 
+          title="OFFICE" 
+          body="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio, culpa." 
+          backgroundImage="/images/furniture/office.png" 
+          backgroundPosition="-340px"
+        />
+        <Card 
+          title="OUTDOOR" 
+          body="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio, culpa." 
+          backgroundImage="/images/furniture/outdoor.png" 
+          backgroundPosition="-500px"
+        />
+      </div>
     </div>
   );
 };
