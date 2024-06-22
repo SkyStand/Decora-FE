5ce6679a06e9675dff9a2d873c0d5aa34688019f
Avriella <avriellasofianti.jj88@gmail.com>
2024-06-22 09:33:29 +0700

    feat : integrate products

--- data/interface/response/IProduct.ts
+++ data/interface/response/IProduct.ts
@@ -1,30 +1,27 @@
-export default interface IProducts {
-    data: IProduct[]
-}
 
-export interface IProduct {
-    id: number
-    image: string
-    name: string
-    description: string
-    type: string
-    category: string
-    style: string
-    berat?: number
-    panjang_product?: number
-    lebar_product?: number
-    tinggi_product?: number
-    variants: Variant[]
+export default interface IProduct {
+    id: number;
+    image: string;
+    name: string;
+    description: string;
+    type: string;
+    category: string;
+    style: string;
+    berat?: number | null;
+    panjang_product?: number | null;
+    lebar_product?: number | null;
+    tinggi_product?: number | null;
+    variants: Variant[];
 }
 
 export interface Variant {
-    id: number
-    product_id: number
-    image: string
-    variant_name: string
-    price: number
-    diskon: number
-    qty: number
-    created_at: string
-    updated_at: string
+    id: number;
+    product_id: number;
+    image: string;
+    variant_name: string;
+    price: number;
+    diskon: number;
+    qty: number;
+    created_at: string;
+    updated_at: string;
 }
--- data/services/ProductService.ts
+++ data/services/ProductService.ts
@@ -1,27 +1,27 @@
 import { AxiosInstance, AxiosResponse } from 'axios';
+import IProduct from '../interface/response/IProduct';
 import cookies from 'js-cookie'
 import instance from '../../helpers/AxiosInstance';
-import IProducts from '../interface/response/IProduct';
 export default class ProductService {
     public constructor(
         private readonly axiosInstance: AxiosInstance = instance
     ) { }
 
-    async fetch(): Promise<IProducts[]> {
+    async fetch(): Promise<IProduct[]> {
         const token = cookies.get('authToken')
-        return (await this.axiosInstance.post('product', null, {
+        return (await this.axiosInstance.get('products', {
+            headers: {
+                "Authorization": `Bearer ${token}`
+            }
+        })).data.data
+    }
+
+    async getImagePath(): Promise<string> {
+        const token = cookies.get('authToken')
+        return (await this.axiosInstance.get('image/product', {
             headers: {
                 "Authorization": `Bearer ${token}`
             }
         })).data
     }
-
-    // async detail(product_id: number): Promise<IProductDetails> {
-    //     const token = cookies.get('authToken')
-    //     return (await this.axiosInstance.post(`product/${product_id}`, null, {
-    //         headers: {
-    //             "Authorization": `Bearer ${token}`
-    //         }
-    //     })).data
-    // }
 }
